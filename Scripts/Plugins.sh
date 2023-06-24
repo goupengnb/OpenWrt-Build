54fd57a1229b4c5c01909e1dd70ab233e3a2beed
goupengnb <105686737+goupengnb@users.noreply.github.com>
2023-06-24 22:57:03 +0800

    更新 Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -17,8 +17,6 @@ git clone --depth=1 --single-branch --branch "luci" https://github.com/xiaorouji
 git clone --depth=1 --single-branch --branch "packages" https://github.com/xiaorouji/openwrt-passwall.git ./pw_packages
 #adguardhome
 git clone --depth=1 --single-branch https://github.com/rufengsuixing/luci-app-adguardhome.git
-＃ddnsgo
-git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
 export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
