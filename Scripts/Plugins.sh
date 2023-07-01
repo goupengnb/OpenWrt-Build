486c513e2e989b521f12fc5990836202287ea039
goupengnb <105686737+goupengnb@users.noreply.github.com>
2023-07-02 00:18:16 +0800

    更新 Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Argon Theme
-git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iq "lede" && echo "18.06" || echo "master") git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 git clone --depth=1 --single-branch git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
 #Pass Wall 
 git clone --depth=1 --single-branch --branch "luci" https://github.com/xiaorouji/openwrt-passwall.git ./pw_luci 
