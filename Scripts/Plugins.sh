a01e03ce8d530226f3ae434d7ea62d4783f62b2d
goupengnb <105686737+goupengnb@users.noreply.github.com>
2023-07-02 13:08:03 +0800

    更新 Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Argon Theme
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git
 #Pass Wall 
 git clone --depth=1 --single-branch --branch "luci" https://github.com/xiaorouji/openwrt-passwall.git ./pw_luci 
 git clone --depth=1 --single-branch --branch "packages" https://github.com/xiaorouji/openwrt-passwall.git ./pw_packages
 #adguardhome
 git clone --depth=1 --single-branch https://github.com/rufengsuixing/luci-app-adguardhome.git
 #ddns-go
-git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-ddns-go.git ./package/ddns-go
+git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-ddns-go.git
