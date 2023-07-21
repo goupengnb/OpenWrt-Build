d3da5845e280059ddaf539b266da9f86c7dae3ff
goupengnb <105686737+goupengnb@users.noreply.github.com>
2023-07-21 23:22:38 +0800

    更新 Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #删除冲突插件
-rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\).*")
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|ttyd\).*")
 #Argon Theme 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
